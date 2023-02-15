f678200727049b555f7cb1773f2011346e3ba71f
ana <andelamo@myuax.com>
2023-02-15 15:41:39 +0100

    Parte3_VentanaBien

--- src/main/java/refactoring_guru/abstract_factory/example/Main.java
+++ src/main/java/refactoring_guru/abstract_factory/example/Main.java
@@ -1,8 +1,12 @@
 package refactoring_guru.abstract_factory.example;
 
+import refactoring_guru.abstract_factory.example.app.Application;
+import refactoring_guru.abstract_factory.example.factories.MacOSFactory;
+import refactoring_guru.abstract_factory.example.factories.WindowsFactory;
+
 import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame{
 
 
     private JButton macOSButtonButton;
@@ -10,13 +14,30 @@ public class Main {
 
     private JPanel panel1;
 
+    public Main(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        macOSButtonButton.addActionListener(e -> {
+            System.out.println("macOSButtonButton");
+        });
+        windowsButtonButton.addActionListener(e -> {
+            System.out.println("windowsButtonButton");
+        });
+    }
+
     public static void main(String[] args) {
-        JFrame frame = new JFrame("main");
+        JFrame frame = new Main();
         frame.setContentPane(new Main().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
+
+
+
+
+
+
+
 
 
 
